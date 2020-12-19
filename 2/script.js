a7d8722bf60b394582fcc327793a87219195bcee
ojc1234 <ojc123004@gmail.com>
2020-12-20 04:09:06 +0900

    newfunction

---
+++ 2/script.js
@@ -0,0 +1,7 @@
+function mode(china , korea , america ,koreatop)
+{
+    document.getElementById("china").style.height=china;
+    document.getElementById("korea").style.height=korea;
+    document.getElementById("america").style.height=america;
+    document.getElementById("korea").style.top=koreatop;
+}
